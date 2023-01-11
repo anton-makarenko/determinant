e68ed5f8dc017e5a4691808d0858df6325d99218
Anton Makarenko <makarenkoanton2013@gmail.com>
2023-01-11 19:13:41 +0200

    Array2D dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// use array2d::Array2D;
+//
+// pub fn delimiter(array2d: Array2D<>)
