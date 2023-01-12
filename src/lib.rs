2858e62979e8ee63bbe975311527af575321b9b5
Anton Makarenko <makarenkoanton2013@gmail.com>
2023-01-12 21:50:39 +0200

    Deleted extra comments and pass a ref instead of copy

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 use array2d::Array2D;
 use num_traits::{Num, NumAssignOps, Zero};
 
-pub fn delimiter<T: NumAssignOps + Num + Display + Copy + Zero>(array2d: Array2D<T>) -> T {
+pub fn delimiter<T: NumAssignOps + Num + Display + Copy + Zero>(array2d: &Array2D<T>) -> T {
     let n = array2d.num_rows();
     return if n == 2 {
         array2d.get(0, 0).copied().unwrap() * array2d.get(1, 1).copied().unwrap()
@@ -10,13 +10,11 @@ pub fn delimiter<T: NumAssignOps + Num + Display + Copy + Zero>(array2d: Array2D
     } else {
         let mut result: T = Zero::zero();
         let mut arrays: Vec<Array2D<T>> = Vec::with_capacity(n);
-        // let mut rows = Vec::with_capacity(n);
-        // let rows_iter = array2d.rows_iter();
         let rows_before: Vec<Vec<T>> = array2d.rows_iter()
             .map(|v| v.map(|x| *x).collect())
             .collect();
         let mut rows_after: Vec<Vec<Vec<T>>> = Vec::with_capacity(n);
-        for i in 0..rows_after.capacity() {
+        for _ in 0..rows_after.capacity() {
             rows_after.push(rows_before.clone());
         }
         for i in 0..rows_after.capacity() {
@@ -25,22 +23,18 @@ pub fn delimiter<T: NumAssignOps + Num + Display + Copy + Zero>(array2d: Array2D
         for i in 0..rows_after.capacity() {
             for j in 0..(rows_after[i].capacity() - 1) {
                 rows_after[i][j].remove(i);
-            } // TODO fix panic
+            }
         }
         for i in 0..rows_after.capacity() {
             arrays.push(Array2D::from_rows(&rows_after[i]).unwrap());
         }
         for i in 0..n {
             if (1 + i) % 2 == 0 {
-                result -= rows_before[0][i] * delimiter(arrays[i].clone());
+                result -= rows_before[0][i] * delimiter(&arrays[i]);
             } else {
-                result += rows_before[0][i] * delimiter(arrays[i].clone());
+                result += rows_before[0][i] * delimiter(&arrays[i]);
             }
         }
         result
     }
-    // for ri in rows_iter {
-    //     let row: Vec<T> = ri.map(|x| *x).collect();
-    //     rows.push(row);
-    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use determinant::delimiter;
 
 fn main() {
     let rows = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
-    println!("{}", delimiter(Array2D::from_rows(&rows).unwrap()));
+    println!("{}", delimiter(&Array2D::from_rows(&rows).unwrap()));
 }
